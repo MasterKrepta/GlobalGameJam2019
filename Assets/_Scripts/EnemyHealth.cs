2aca569473680bc0128fee97f7bed09c81a3c18b
MasterKrepta <brianj.pajak@gmail.com>
2019-01-26 23:10:11 -0500

    powerup placements

--- Assets/_Scripts/EnemyHealth.cs
+++ Assets/_Scripts/EnemyHealth.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour, IDamageable
 {
     [SerializeField]Enemy stats;
-
+    [SerializeField] List<Transform> spawnables = new List<Transform>();
     [SerializeField]float currentHealth;
 
     private float flashTime = .2f;
@@ -19,8 +19,21 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 
 
     public void Die() {
+        ChanceForLoot();
         Destroy(this.gameObject);
         //TODO give points or spawn next one
+    }
+
+    private void ChanceForLoot() {
+        System.Random rnd = new System.Random();
+        int val = rnd.Next(1, 10);
+        if (val < 6) {
+            Instantiate(spawnables[UnityEngine.Random.Range(0, spawnables.Count - 1)], transform.position, Quaternion.identity);
+            Debug.Log("we spawned loot");
+            //50% probabilty
+            // 1/10 + 1/10 + 1/10 + 1/10 + 1/10 + 1/10 = 5/10 = 0.5
+        }
+
     }
 
     public void TakeDamage(float dmg) {
